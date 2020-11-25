11de0e35f0148b9f950dedb16c54220970c2452a
Adnan Ahmad <adnanahmad4873@gmail.com>
2020-11-25 15:19:43 -0800

    changed dummy value to rax

--- src/runtime/asmOutput.s
+++ src/runtime/asmOutput.s
@@ -5,47 +5,9 @@ _asm_main:
     pushq %rbp
     movq %rsp,%rbp
 
-    movq $8,%rdi
-    call _mjcalloc
-    leaq Two$$(%rip),%rdx
-    movq %rdx,0(%rax)
-
-    movq %rax,%rdi
-    movq 0(%rdi),%rax
-    call *16(%rax)
-
-    movq %rax,%rdi
-    call _put
-
-    movq %rbp,%rsp
-    popq %rbp
-    ret
-
-Two$add:
-    pushq %rbp
-    movq %rsp,%rbp
-
-    movq $1,%rax
-    pushq %rax
-    movq $1,%rax
-    popq %rdx
-    addq %rdx,%rax
-
-    movq %rbp,%rsp
-    popq %rbp
-    ret
-
-Two$seventeen:
-    pushq %rbp
-    movq %rsp,%rbp
-
-    movq $1,%rax
-    movq %rax,%rdi
-    call _put
-
-
     movq $3,%rax
-    pushq %rax
+pushq %rax
+popq %rsi
     movq $8,%rdi
     call _mjcalloc
     leaq Two$$(%rip),%rdx
@@ -55,35 +17,26 @@ Two$seventeen:
     movq 0(%rdi),%rax
     call *8(%rax)
 
-    popq %rdx
-    imulq %rdx,%rax
+    movq %rax,%rdi
+    call _put
 
+    movq %rbp,%rsp
+    popq %rbp
+    ret
+
+Two$first:
+    pushq %rbp
+    movq %rsp,%rbp
+
+    subq $8,%rsp
+    subq $8,%rsp
+    movq %rax,-8(%rbp)
     movq %rax,%rdi
     call _put
 
 
-    movq $9,%rax
-    pushq %rax
-    movq $3,%rax
-    pushq %rax
     movq $1,%rax
-    popq %rdx
-    subq %rax,%rdx
-    movq %rdx,%rax
-
-    popq %rdx
-    imulq %rdx,%rax
-
-    pushq %rax
-    movq $5,%rax
-    popq %rdx
-    addq %rdx,%rax
-
-    movq %rax,%rdi
-    call _put
-
-
-    movq $17,%rax
+addq $16,%rsp
     movq %rbp,%rsp
     popq %rbp
     ret
@@ -91,5 +44,4 @@ Two$seventeen:
 .data
 Two$$:
     .quad 0
-    .quad Two$add
-    .quad Two$seventeen
+    .quad Two$first
